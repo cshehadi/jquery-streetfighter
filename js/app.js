e004ae5d272cdba501c5a0ac5465fe60df7f0051
Charlie Shehadi <cshehadi@gmail.com>
2016-06-09 15:37:53 -0400

    add another fighter

--- js/app.js
+++ js/app.js
@@ -1,41 +1,34 @@
 $(document).ready(function() {
 
-    $('.ryu').mouseenter(function() {
-        activate('ryu-ready');
+    $('.ryu, .hulk-ryu').mouseenter(function() {
+        activate('ryu-ready', $(this));
     })
         .mouseleave(function() {
-            activate('ryu-still');
+            activate('ryu-still', $(this));
         })
         .mousedown(function() {
             playHadouken();
-            activate('ryu-throwing');
-            $('.hadouken').finish().show().animate(
-                {'left': '1020px'},
-                500,
-                function() {
-                    $(this).hide();
-                    $(this).css('left', '520px');
-                }
-            );
+            activate('ryu-throwing', $(this));
+            throwHadouken($(this));
         })
         .mouseup(function() {
-            activate('ryu-ready');
+            activate('ryu-ready', $(this));
         });
 
     $(document).keydown(function(event) {
         if (event.which == 88)
-            activate('ryu-cool',true); 
+            activate('ryu-cool', $(this), true); 
     })
         .keyup(function() {
-            activate(active_class);
+            activate(active_class, $(this));
         });
 
 });
 
 var active_class = 'ryu-still';
 
-function activate(class_to_activate, temporary = false) {
-    $('.ryu-image').each(function() {
+function activate(class_to_activate, elem, temporary = false) {
+    $('.ryu-image', elem).each(function() {
         if ($(this).hasClass(class_to_activate)) {
             $(this).show();
             if (!temporary)
@@ -44,6 +37,42 @@ function activate(class_to_activate, temporary = false) {
             $(this).hide();
         }
     });
+}
+
+function throwHadouken(elem) {
+    var left;
+    var dest;
+    var opponent;
+
+    if (elem.attr('class') == 'ryu') {
+        left = '500px';
+        dest = '920px';
+        opponent = 'hulk-ryu';
+        impact_dir = 'left';
+        $('.hadouken').css('left', left); // set start position
+        $('.hadouken').removeClass('flipped'); // set direction
+    } else {
+        left = '1020px';
+        dest = '270px';
+        opponent = 'ryu';
+        impact_dir = 'right';
+        $('.hadouken').css('left', left);
+        $('.hadouken').addClass('flipped');
+    }
+    $('.hadouken').finish().show().animate(
+        {'left': dest},
+        500,
+        function() {
+            if (opponent == 'hulk-ryu') {
+                $(this).parent().animate({'padding-right': '0px'},50);
+                $(this).parent().animate({'padding-right': '10px'},50);
+            } else {
+                $(this).parent().animate({'padding-left': '0px'},50);
+                $(this).parent().animate({'padding-left': '10px'},50);
+            }
+            $(this).hide();
+        }
+    );
 }
 
 function playHadouken () {
